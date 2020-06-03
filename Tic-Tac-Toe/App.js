cffa1b635e9d49e03e29cc090ed96b22ec171960
ltthinh1999 <thinhle111199@gmail.com>
2020-06-03 13:07:02 +0700

    Reworked on Mac

---
+++ Tic-Tac-Toe/App.js
@@ -0,0 +1,249 @@
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity, Dimensions, StyleSheet } from 'react-native';
+import aiTurn from './components/AI';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mode: "bot",
+      running: true,
+      playerTurn: true,
+      winner: 0
+    };
+
+    this.entities = this.setup();
+  }
+
+  setup = () => {
+    let table = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ];
+
+    let turns = [];
+
+    return {
+      table: table,
+      turns: turns
+    }
+  }
+
+  reset = () => {
+    this.entities = this.setup();
+    this.setState({
+      mode: "bot",
+      running: true,
+      playerTurn: true,
+      winner: 0
+    });
+  }
+
+  duel = (key) => {
+    let x = Math.floor(key / 3);
+    let y = key % 3;
+    if (this.entities.table[x][y] == 0) {
+      let move = this.state.playerTurn ? -1 : 1;
+      this.entities.table[x][y] = move;
+      this.entities.turns.push(key);
+      this.setState(prevState => ({
+        playerTurn: !prevState.playerTurn
+      }));
+      let winner = this.check(x, y, move);
+      if (winner != 999) {
+        this.setState({
+          running: false,
+          winner: winner
+        })
+        return;
+      };
+    }
+  }
+
+  tick = key => event => {
+    if (this.state.mode == "duel") {
+      this.duel(key);
+    }
+    else if (this.state.mode = "bot") {
+      if (this.state.playerTurn) {
+        let x = Math.floor(key / 3);
+        let y = key % 3;
+        if (this.entities.table[x][y] == 0) {
+          this.entities.table[x][y] = -1;
+          this.entities.turns.push({ x, y });
+          let winner = this.check(x, y, -1);
+          if (winner != 999) {
+            this.setState({
+              running: false,
+              winner: winner
+            })
+            return;
+          };
+          this.setState({
+            playerTurn: false
+          });
+          let ai = aiTurn(this.entities.table);
+          this.entities.turns.push({ ai });
+          winner = this.check(ai.i, ai.j, 1);
+          if (winner != 999) {
+            this.setState({
+              running: false,
+              winner: winner
+            })
+            return;
+          };
+          this.setState({
+            playerTurn: true
+          });
+        }
+      }
+    }
+  }
+
+  check(x, y, move) {
+    let col, row, dia, rdia;
+    col = row = dia = rdia = 0;
+    for (let i = 0; i < 3; i++) {
+      if (this.entities.table[x][i] == move) {
+        col++;
+      }
+      if (this.entities.table[i][y] == move) {
+        row++;
+      }
+      if (this.entities.table[i][i] == move) {
+        dia++;
+      }
+      if (this.entities.table[i][3 - (i + 1)] == move) {
+        rdia++;
+      }
+    }
+    if (row == 3 || col == 3 || dia == 3 || rdia == 3) {
+      return move;
+    }
+    if (this.entities.turns.length == 9) {
+      return 0;
+    }
+    return 999;
+  }
+
+  renderMove(key) {
+    if (this.entities.table[Math.floor(key / 3)][key % 3] != 0) {
+      if (this.entities.table[Math.floor(key / 3)][key % 3] == 1) {
+        return <Text style={styles.O}>O</Text>;
+      }
+      return <Text style={styles.X}>X</Text>;
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.wrapper}>
+        <View style={styles.container}>
+          <TouchableOpacity activeOpacity={1} key={0} style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0 }]} onPress={this.tick(0)}>
+            {this.renderMove(0)}
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={1} key={1} style={[styles.tile, { borderTopWidth: 0 }]} onPress={this.tick(1)}>
+            {this.renderMove(1)}
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={1} key={2} style={[styles.tile, { borderRightWidth: 0, borderTopWidth: 0 }]} onPress={this.tick(2)}>
+            {this.renderMove(2)}
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity activeOpacity={1} key={3} style={[styles.tile, { borderLeftWidth: 0 }]} onPress={this.tick(3)}>
+            {this.renderMove(3)}
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={1} key={4} style={styles.tile} onPress={this.tick(4)}>
+            {this.renderMove(4)}
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={1} key={5} style={[styles.tile, { borderRightWidth: 0 }]} onPress={this.tick(5)}>
+            {this.renderMove(5)}
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity activeOpacity={1} key={6} style={[styles.tile, { borderLeftWidth: 0, borderBottomWidth: 0 }]} onPress={this.tick(6)}>
+            {this.renderMove(6)}
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={1} key={7} style={[styles.tile, { borderBottomWidth: 0 }]} onPress={this.tick(7)}>
+            {this.renderMove(7)}
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={1} key={8} style={[styles.tile, { borderRightWidth: 0, borderBottomWidth: 0 }]} onPress={this.tick(8)}>
+            {this.renderMove(8)}
+          </TouchableOpacity>
+        </View>
+        {!this.state.running && <TouchableOpacity style={styles.gameOverWrapper} onPress={this.reset}>
+          <View style={styles.gameOverView}>
+            {this.state.winner == 0 && <Text style={[styles.gameOverText, { fontSize: 24 }]}>Draw</Text>}
+            {this.state.winner == 1 && <Text style={[styles.gameOverText, { fontSize: 24 }]}>O wins</Text>}
+            {this.state.winner == -1 && <Text style={[styles.gameOverText, { fontSize: 24 }]}>X wins</Text>}
+            <Text style={[styles.gameOverText, { fontSize: 12 }]}>Tap to play again</Text>
+          </View>
+        </TouchableOpacity>}
+      </View>
+    )
+  };
+}
+
+const styles = StyleSheet.create({
+
+  wrapper: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  container: {
+    flexDirection: "row",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  gameOverWrapper: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    flex: 1
+  },
+
+  gameOverView: {
+    top: Dimensions.get("screen").height / 2 - 24,
+    bottom: Dimensions.get("screen").height / 2 + 48,
+    left: 0,
+    right: 0,
+    backgroundColor: "black",
+    opacity: 0.5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  gameOverText: {
+    color: '#fff'
+  },
+
+  tile: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: Dimensions.get("screen").width / 3,
+    height: Dimensions.get("screen").width / 3,
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: 'white'
+  },
+
+  O: {
+    color: "green",
+    fontSize: Dimensions.get("screen").width / 3,
+  },
+
+  X: {
+    color: "red",
+    fontSize: Dimensions.get("screen").width / 3,
+  }
+})
---
+++ Tic-Tac-Toe/components/AI.js
@@ -0,0 +1,97 @@
+function isEqual(a, b, c) {
+    return a == b && b == c && a != 0;
+}
+
+function checkState(table) {
+    let winner = 999;
+
+    for (let i = 0; i < 3; i++) {
+        if (isEqual(table[i][0], table[i][1], table[i][2])) {
+            winner = table[i][0];
+        }
+    }
+
+    for (let i = 0; i < 3; i++) {
+        if (isEqual(table[0][i], table[1][i], table[2][i])) {
+            winner = table[0][i];
+        }
+    }
+
+    if (isEqual(table[0][0], table[1][1], table[2][2])) {
+        winner = table[0][0];
+    }
+    if (isEqual(table[2][0], table[1][1], table[0][2])) {
+        winner = table[2][0];
+    }
+    let turnsLeft = 0;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (table[i][j] == 0) {
+                turnsLeft++;
+            }
+        }
+    }
+    if (winner == 999 && turnsLeft == 0) {
+        return 0;
+    }
+    return winner;
+}
+
+function aiTurn(table) {
+    let score = -Infinity;
+    let bestMove;
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (table[i][j] == 0) {
+                table[i][j] = 1;
+                let tmp = minimax(table, 0, false);
+                table[i][j] = 0;
+                if (tmp > score) {
+                    score = tmp;
+                    bestMove = { i, j };
+                }
+            }
+        }
+    }
+
+    table[bestMove.i][bestMove.j] = 1;
+    return bestMove;
+}
+
+function minimax(table, depth, isMax) {
+    let winner = checkState(table);
+    if (winner != 999) {
+        return winner;
+    }
+
+    if (isMax) {
+        let score = -Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (table[i][j] == 0) {
+                    table[i][j] = 1;
+                    let tmp = minimax(table, depth + 1, false);
+                    table[i][j] = 0;
+                    score = Math.max(tmp, score);
+                }
+            }
+        }
+        return score;
+    } else {
+        let score = Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (table[i][j] == 0) {
+                    table[i][j] = -1;
+                    let tmp = minimax(table, depth + 1, true);
+                    table[i][j] = 0;
+                    score = Math.min(tmp, score);
+                }
+            }
+        }
+        return score;
+    }
+}
+
+export default aiTurn;
